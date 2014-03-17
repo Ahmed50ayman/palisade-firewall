b5c077f8aa0eb3871dc0cbd421d2ba895767c2bb
bova <povetkin84@gmail.com>
2014-03-18 00:45:53 +0500

    ipt_rules displaying from $resource

--- palisade/ui/static/js/controllers.js
+++ palisade/ui/static/js/controllers.js
@@ -1,8 +1,16 @@
 /**
  * Created by bova on 17.03.14.
  */
-var palisadeFirewallApp = angular.module('palisadeFirewallApp', ['ngRoute', 'iptServices']);
-palisadeFirewallApp.controller('IPTRuleListCtrl', ['$scope', 'IPTRule', function ($scope, IPTRule) {
-    $scope.ipt_rules = IPTRule.query();
+var palisadeFirewallApp = angular.module('palisadeFirewallApp', ['iptServices', 'ngResource']);
+palisadeFirewallApp.controller('IPTRuleListCtrl', ['$resource', '$scope', 'IPTRule', function ($resource, $scope, IPTRule) {
+//    $scope.ipt_rules = IPTRule.query(function(){});
     $scope.orderProp = 'id';
+    var rulesCollection =  $resource("/api/ipt_rule/",{}, {
+            'query': {method: 'GET', responseType: 'json', isArray: true
+                , transformResponse: function(data, headersGetter) {return data.objects;}
+            }
+        });
+    $scope.my_rules = rulesCollection.query();
+    console.log($scope.my_rules);
+    $scope.jopa = 'JOPA';
 }]);
--- palisade/ui/static/js/services.js
+++ palisade/ui/static/js/services.js
@@ -6,6 +6,10 @@ var iptServices = angular.module('iptServices', ['ngResource']);
 iptServices.factory('IPTRule', ['$resource',
     function($resource){
         return $resource('api/ipt_rule/', {}, {
-            query: {method: 'GET', params:{id: 'id'}, isArray:false}
+            'query': {method: 'GET', isArray: true, transformResponse: function(data, headersGetter) {
+                return data.objects;
+            }}
         });
     }]);
+
+// params:{id: 'id'}, isArray:true
